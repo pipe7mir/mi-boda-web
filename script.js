1a906601c1b364e1faf7a425b0bb51401f28fec6
Pipe Miranda <felipemiranda959@gmail.com>
2026-01-10 05:25:39 -0500

    inicio de script js

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+console.log("¡Bienvenido a la web de nuestra boda!");
+
+// Aquí pondremos la lógica de la cuenta regresiva más adelante
+function saludarInvitados() {
+    alert("¡Gracias por visitar nuestra página! Pronto subiremos más detalles.");
+}
+
+// Puedes probar llamando a esta función desde la consola del navegador
